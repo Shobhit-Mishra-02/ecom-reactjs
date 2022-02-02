426e9cc5afeb210a6c7850d4980473cea6821320
shobhit <shobhitmishra2002@gmail.com>
2022-02-02 12:20:02 +0530

    got a bug in makingRequest function in the client side

--- frontend/src/Cardsection.js
+++ frontend/src/Cardsection.js
@@ -1,23 +1,35 @@
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const Cardsection = () => {
+  const [prodData, setProdData] = useState([]);
+
+  const makingRequest = async () => {
+    const data = await fetch("http://localhost:5000/getallprod");
+    const json = await data.json();
+    return json.products;
+  };
+
+  useEffect(() => {
+    setProdData(makingRequest());
+  }, []);
+
   return (
     <div className="container d-flex justify-content-center align-items-center">
-      <Card
-        title="watch"
-        desc="yes it is working here, hey what are you doing here, you should be in your home."
-        img="1643781636563-22033305-watch.jpg"
-      ></Card>
-      <Card
-        title="watch"
-        desc="yes it is working here, hey what are you doing here, you should be in your home."
-        img="1643781636563-22033305-watch.jpg"
-      ></Card>
-      <Card
-        title="watch"
-        desc="yes it is working here, hey what are you doing here, you should be in your home."
-        img="1643781636563-22033305-watch.jpg"
-      ></Card>
+      {prodData.length ? (
+        prodData.map((item) => {
+          return (
+            <Card
+              key={item.productTitle}
+              title={item.productTitle}
+              desc={item.productDesc}
+              img={item.productImg}
+            />
+          );
+        })
+      ) : (
+        <div>loading</div>
+      )}
     </div>
   );
 };
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -5,11 +5,7 @@ const Home = () => {
   return (
     <div>
       <Navigation />
-      {/* <img
-        src="http://localhost:5000/1643724667349-747440153-pexels-steve-johnson-1269968.jpg"
-        width="400px"
-        height="400px"
-      ></img> */}
+
       <Cardsection />
     </div>
   );
